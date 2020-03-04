84d772a42f89a0cfafa8330c0cb4ca5f930aa6bc
LidiCo <lzakenbsn@gmail.com>
2020-03-04 10:54:56 +0200

    Ex3

---
+++ LargestPrimeFactor-Ex3.py
@@ -0,0 +1,22 @@
+import math
+
+def largerPrimeFactor(number):
+    maxPrime = 0
+
+    #Handle the case number is Even
+    while number % 2 == 0:
+        maxPrime = 2
+        number /= 2
+
+    #Skips the even numbers
+    for i in range(3, int(math.sqrt(number)) + 1, 2):
+        while number % i == 0:
+            maxPrime = i
+            number = number / i
+    # This condition is to handle the case when n is a prime number greater than 2
+    if number > 2:
+        maxPrime = number
+
+    return int(maxPrime)
+
+print(largerPrimeFactor(60))
