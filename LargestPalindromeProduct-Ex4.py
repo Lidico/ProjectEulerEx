f0304fd51715f22521f3d507c9ff24518bcdfda0
LidiCo <lzakenbsn@gmail.com>
2020-03-04 11:59:53 +0200

    Ex4

--- LargestPalindromeProduct-Ex4.py
+++ LargestPalindromeProduct-Ex4.py
@@ -1,7 +1,21 @@
-import math
 
 def largerPalindromProduct(n):
-    numbers = (pow(10,n)-1, pow(10,n)-1)
-    print(numbers)
+    numbers = [pow(10,n)-1, pow(10,n)-1]
+    count = 0
+    for i in range(numbers[0], 0, -1):
+        count += 1
+        if count == 100:
+            numbers[0] = pow(10,n)-1
+            numbers[1] -= 1
+            count = 0
+        res = str(numbers[0]*numbers[1]) == str(numbers[0]*numbers[1])[::-1]
+        if res:
 
-largerPalindromProduct(3)
+            return numbers
+        else:
+            numbers[0] -= 1
+
+
+
+twoLargerNums = largerPalindromProduct(3)
+print(twoLargerNums[0], "*", twoLargerNums[1], "=", twoLargerNums[0]*twoLargerNums[1])
