9f5a222242c8d6d0d0972a195a31720bf77e4ef9
LidiCo <lzakenbsn@gmail.com>
2020-03-18 13:00:58 +0200

    Ex16

---
+++ powerDigitSum=Ex16.py
@@ -0,0 +1,15 @@
+"""
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2^1000?
+"""
+import math
+from functools import reduce
+from pip._vendor.pytoml.writer import long
+
+
+def powerDigitalSum(n):
+    number = str(long(math.pow(2, n)))
+    return reduce(lambda x, y : int(x) + int(y), number)
+
+
+print(powerDigitalSum(1000))
