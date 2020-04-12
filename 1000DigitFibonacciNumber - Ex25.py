e8017bafcdf68346461ca7a70c811cd6c10337fe
LidiCo <lzakenbsn@gmail.com>
2020-04-12 15:06:58 +0300

    Ex26

--- 1000DigitFibonacciNumber - Ex25.py
+++ 1000DigitFibonacciNumber - Ex25.py
@@ -17,6 +17,8 @@ F12 = 144
 The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 """
+from pip._vendor.pytoml.writer import long
+
 
 def nDigitsFibonacci(n):
     listOfFibonacci = [1, 1, 2]
